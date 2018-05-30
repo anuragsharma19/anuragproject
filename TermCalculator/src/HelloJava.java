50190dbb1ef6aea29c2b978cec27aaddf02c04be
anuragsharma19 <anuragsharma19@hotmail.com>
2018-05-30 15:36:09 -0700

    commit 3

--- TermCalculator/src/HelloJava.java
+++ TermCalculator/src/HelloJava.java
@@ -3,7 +3,7 @@ public class HelloJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello Java for Git Hub Integration 1");
+System.out.println("Hello Java for Git Hub Integration");
 	}
 
 }
